125d606e280db28ade4688b835d4fd2144179cc5
sammorton9@hotmail.co.uk <>
2022-11-30 10:57:17 +0000

    Basic implementation of LineRenderer

--- Assets/Scripts/LaserPointerScript.cs
+++ Assets/Scripts/LaserPointerScript.cs
@@ -1,18 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using UnityEngine;
 
 public class LaserPointerScript : MonoBehaviour
 {
-    public LineRenderer lineRenderer;
-    public float laserWidth = 0.3f;
+    public float laserWidth;
     public float maxLength = 28f;
+
+    private Vector3[] newPositions;
+    private LineRenderer lineRenderer;
+    private Vector3 startPoint = Vector3.zero;
+    private Vector3 endPoint = Vector3.zero;
 
     // Start is called before the first frame update
     void Start()
     {
-        Vector3[] initLaserPositions = new Vector3[2] { Vector3.zero, Vector3.zero };
-        lineRenderer.SetPositions(initLaserPositions);
+        lineRenderer = GetComponent<LineRenderer>();
+        newPositions = new Vector3[2] { startPoint, endPoint };
+
+        lineRenderer.SetPositions(newPositions);
         lineRenderer.startWidth = laserWidth;
         lineRenderer.endWidth = laserWidth;
     }
@@ -20,6 +27,15 @@ public class LaserPointerScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        endPoint = startPoint;
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            endPoint.x += maxLength;
+        }
+
+        newPositions[0] = startPoint;
+        newPositions[1] = endPoint;
+        lineRenderer.SetPositions(newPositions);
     }
 }
