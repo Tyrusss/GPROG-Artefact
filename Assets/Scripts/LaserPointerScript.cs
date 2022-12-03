0dd2aa562c89b93671909f7c71066f8ed4257ec5
sammorton9@hotmail.co.uk <>
2022-12-03 21:59:53 +0000

    Rotate laser pointer towards mouse (clamped to 60 degrees of allowance on forward) Spacebar RayCasts laser and LineRenderer updates positions to end at rayHit.point

--- Assets/Scripts/LaserPointerScript.cs
+++ Assets/Scripts/LaserPointerScript.cs
@@ -6,18 +6,36 @@ using UnityEngine;
 public class LaserPointerScript : MonoBehaviour
 {
     public float laserWidth;
-    public float maxLength = 28f;
+    public float maxLength;
+    public GameObject PlayerCharacter;
 
+    [Header("Serialized fields")]
+    [SerializeField]
+    private PlayerController playerController;
+
+    [SerializeField]
     private Vector3[] newPositions;
+    private Vector3[] defaultPositions;
+    [SerializeField]
     private LineRenderer lineRenderer;
+
+    [SerializeField]
     private Vector3 startPoint = Vector3.zero;
-    private Vector3 endPoint = Vector3.zero;
+    [SerializeField]
+    private Vector3 endPoint;
+    private Vector3 defaultEndPoint;
+
+    private Vector3 mousePos;
+    private float angle;
 
     // Start is called before the first frame update
     void Start()
     {
         lineRenderer = GetComponent<LineRenderer>();
-        newPositions = new Vector3[2] { startPoint, endPoint };
+        playerController = PlayerCharacter.GetComponent<PlayerController>();
+
+        defaultEndPoint = new Vector3(maxLength, 0, 0);
+        defaultPositions = new Vector3[2] { startPoint, defaultEndPoint };
 
         lineRenderer.SetPositions(newPositions);
         lineRenderer.startWidth = laserWidth;
@@ -27,15 +45,39 @@ public class LaserPointerScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        endPoint = startPoint;
+        endPoint = defaultEndPoint;
+        newPositions = defaultPositions;
+
+        mousePos = playerController.mousePos;
+
+        if (lineRenderer.enabled)
+        {
+            lineRenderer.enabled = false;
+        }
+
+        if (playerController.rayHit.collider != null)
+        {
+            endPoint = transform.InverseTransformPoint(playerController.rayHitPoint);
+        }
 
         if (Input.GetKey(KeyCode.Space))
         {
-            endPoint.x += maxLength;
+            newPositions[1] = endPoint;
+            lineRenderer.SetPositions(newPositions);
+            lineRenderer.enabled = true;
         }
-
-        newPositions[0] = startPoint;
-        newPositions[1] = endPoint;
-        lineRenderer.SetPositions(newPositions);
+        RotateLaserPointer();
     }
+    void RotateLaserPointer()
+    {
+        // Set world space rotation in direction of mouse pointer
+        angle = Mathf.Atan2(mousePos.y - transform.position.y, mousePos.x - transform.position.x);
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0, 0, angle * Mathf.Rad2Deg), 0.3f);
+
+        // Clamp localRotation to only look forward
+        float localAngle = transform.localRotation.eulerAngles.z;
+        if (localAngle > 60 && localAngle < 180) { transform.localRotation = Quaternion.Euler(0, 0, 60); }
+        if (localAngle < 300 && localAngle > 180) { transform.localRotation = Quaternion.Euler(0, 0, 300); }
+    }
+
 }
